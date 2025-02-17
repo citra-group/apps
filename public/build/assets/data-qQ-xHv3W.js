988b813890ddafd108fb611ebe59426f027d68a9
muemdavid <muammar022@gmail.com>
2025-02-17 15:42:19 +0700

    update

--- public/build/assets/data-qQ-xHv3W.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,b as s,w as o,d as a,o as e}from"./App-fNvSmfwW.js";const f={name:"infrastructure-locations-data"};function p(d,t,i,l,m,u){const n=a("form-data");return e(),s(n,null,{forminfo:o(()=>t[0]||(t[0]=[])),helpdesk:o(()=>t[1]||(t[1]=[])),utility:o(()=>t[2]||(t[2]=[])),_:1})}const c=r(f,[["render",p]]);export{c as default};
--- public/build/assets/desktop-D7itDb_N.js
+++
@@ -1,1 +0,0 @@
-import{c as p,r as s,A as t}from"./App-fNvSmfwW.js";const o=p(t);s(o);o.mount("#monosoft");
