8ddb99a82c1d7210f927197a545dcbaf95a290c2
muemdavid <muammar022@gmail.com>
2025-02-17 11:31:04 +0700

    update

---
+++ public/build/assets/desktop-D7itDb_N.js
@@ -0,0 +1,1 @@
+import{c as p,r as s,A as t}from"./App-fNvSmfwW.js";const o=p(t);s(o);o.mount("#monosoft");
--- public/build/assets/mobile-BeJHlQLL.js
+++
@@ -1,1 +0,0 @@
-import{c as p,a as s,A as a}from"./App-D3npthgT.js";const o=p(a);s(o);o.mount("#monosoft");
