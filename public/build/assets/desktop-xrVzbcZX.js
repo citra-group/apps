b59a86ebf079878fb475e31a222c73c93546efed
muemdavid <muammar022@gmail.com>
2025-03-03 12:47:07 +0700

    update

---
+++ public/build/assets/desktop-xrVzbcZX.js
@@ -0,0 +1,1 @@
+import{c as p,r as s,A as t}from"./App-5lJ7zli5.js";const o=p(t);s(o);o.mount("#monosoft");
---
+++ public/build/assets/mobile-PztU1KUq.js
@@ -0,0 +1,1 @@
+import{c as p,a as s,A as a}from"./App-5lJ7zli5.js";const o=p(a);s(o);o.mount("#monosoft");
