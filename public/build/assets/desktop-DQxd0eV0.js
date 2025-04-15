475893c49be65103965138675549190da24598b3
muemdavid <muammar022@gmail.com>
2025-04-15 17:14:02 +0700

    update

---
+++ public/build/assets/desktop-DQxd0eV0.js
@@ -0,0 +1,1 @@
+import{c as p,r as s,A as t}from"./App-CjoH1pZn.js";const o=p(t);s(o);o.mount("#monosoft");
---
+++ public/build/assets/mobile-DOl_xm_Y.js
@@ -0,0 +1,1 @@
+import{c as p,a as s,A as a}from"./App-CjoH1pZn.js";const o=p(a);s(o);o.mount("#monosoft");
