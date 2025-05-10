eefe87d65c8c13e72a7a0d4b3fe0eb26de85fb55
muemdavid <muammar022@gmail.com>
2025-05-10 13:50:52 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,4 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-class ConsoleKernel
-{
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('coperative:generate-wajib')->daily();
-    }
-}
+Schedule::command('coperative:generate-wajib')->daily();
